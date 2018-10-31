a141b988f2101040d94175dab7f507f6056422b6
nagatech <cheesecakebot@yahoo.co.jp>
2018-10-31 13:03:42 +0900

    limitation

--- main.rb
+++ main.rb
@@ -178,17 +178,25 @@ client.mentions_timeline(count: 10).each do |tweet|
         end
       elsif /\d+$/ === tweet.text
         num = tweet.text.match(/(\d+)$/)
-        po = Prime.prime_division(num[1].to_i)
-        content = ""
-        po.each do |popo|
-          if (popo[1] == 1)
-            content << "#{popo[0]} * "
-          else
-            content << "#{popo[0]}^#{popo[1]} * "
+        if num[1].length < 20
+          po = Prime.prime_division(num[1].to_i)
+          content = ""
+          po.each do |popo|
+            if (popo[1] == 1)
+              content << "#{popo[0]} * "
+            else
+              content << "#{popo[0]}^#{popo[1]} * "
+            end
           end
+          contents = content[0..-3]
+          if /\*/ !== contents && /\^/ !== contents
+            client.update("@#{tweet.user.screen_name} \n素数だよ", options = {:in_reply_to_status_id => tweet.id})
+          else
+            client.update("@#{tweet.user.screen_name} \n#{contents}", options = {:in_reply_to_status_id => tweet.id})
+          end
+        else
+          client.update("@#{tweet.user.screen_name} \nふぇぇ、計算できないよぅ><", options = {:in_reply_to_status_id => tweet.id})
         end
-        contents = content[0..-3]
-        client.update("@#{tweet.user.screen_name} \n#{contents}", options = {:in_reply_to_status_id => tweet.id})
       else
         if (JSTTime.time.sec.to_i % 10) == 0
           client.favorite(tweet.id)
